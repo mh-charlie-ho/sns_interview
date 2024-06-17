f4e7f3db64a335b7ee6087efcc470b215df78b8f
charlie.ho <mth35416tw@gmail.com>
2024-06-18 07:47:33 +0800

    add monitor plot for robot  joint

--- src/robot/robot/arm_robot.py
+++ src/robot/robot/arm_robot.py
@@ -20,6 +20,10 @@ ROBOTYLOWER = -100.0
 
 METER2DEGREE = 1000  # 1mm = 1 degree
 
+# global variable for update plot
+pos1 = 0
+pos2 = 0
+
 
 class ArmRobot(Node):
 
@@ -29,9 +33,16 @@ class ArmRobot(Node):
         g2 = MutuallyExclusiveCallbackGroup()
         g3 = MutuallyExclusiveCallbackGroup()
         g4 = MutuallyExclusiveCallbackGroup()
-        
-        self.botCmdPub = self.create_subscription(JointState, "joint_cmd", self.setBotPos, 10, callback_group=g1)
-        self.botState = self.create_publisher(JointState, "joint_state", 10, callback_group=g2)
+
+        self.botCmdPub = self.create_subscription(JointState,
+                                                  "joint_cmd",
+                                                  self.setBotPos,
+                                                  10,
+                                                  callback_group=g1)
+        self.botState = self.create_publisher(JointState,
+                                              "joint_state",
+                                              10,
+                                              callback_group=g2)
         self.create_timer(0.5, self.pubRobotState, callback_group=g2)
 
         self.create_timer(0.5, self.updateX, callback_group=g3)
@@ -41,13 +52,16 @@ class ArmRobot(Node):
         self.RobotPos = [0, 0]  # rad
 
     def setBotPos(self, msg):
-        self.tRobotPos[0] = msg.position[0] 
+        self.tRobotPos[0] = msg.position[0]
         self.tRobotPos[1] = msg.position[1]
 
     def pubRobotState(self):
+        global pos1, pos2
+        pos1 = self.RobotPos[0]
+        pos2 = self.RobotPos[1]
         jointCmd = JointState()
-        jointCmd.position.append(self.RobotPos[0])
-        jointCmd.position.append(self.RobotPos[1])
+        jointCmd.position.append(pos1)
+        jointCmd.position.append(pos2)
         self.botState.publish(jointCmd)
         time.sleep(0.1)
 
@@ -55,24 +69,25 @@ class ArmRobot(Node):
         r = target
         y = state
 
-        p = 0.5
+        p = 0.1
         diff = r - y
 
         u = p * diff
         y = y + u
         return y
-    
+
     def updateX(self):
-        self.RobotPos[0] = self.updateMotor(self.RobotPos[0], self.tRobotPos[0])
+        self.RobotPos[0] = self.updateMotor(self.RobotPos[0],
+                                            self.tRobotPos[0])
         time.sleep(0.1)
 
     def updateY(self):
-        self.RobotPos[1] = self.updateMotor(self.RobotPos[1], self.tRobotPos[1])
+        self.RobotPos[1] = self.updateMotor(self.RobotPos[1],
+                                            self.tRobotPos[1])
         time.sleep(0.1)
 
 
-
-def main():
+def node():
     rclpy.init()
 
     print("starting robot...")
@@ -83,6 +98,21 @@ def main():
     executor.spin()
 
     rclpy.shutdown()
+
+
+def updatePlot():
+    global pos1, pos2
+    while True:
+        ani.sendDate(pos1, pos2)
+        time.sleep(0.1)
+
+
+def main():
+    global ani
+    threading.Thread(target=node).start()
+    ani = MonitorPlot()
+    threading.Thread(target=updatePlot).start()
+    ani.animate()
 
 
 if __name__ == "__main__":

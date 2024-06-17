83ab369b1270a449690cbf55396b51c32e399cec
charlie.ho <mth35416tw@gmail.com>
2024-06-18 07:46:07 +0800

    monitor:  add label  and legend

--- src/robot/robot/monitor_plot.py
+++ src/robot/robot/monitor_plot.py
@@ -3,31 +3,33 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import time
 
+import threading
+
 
 class MonitorPlot():
 
     def __init__(self):
         self.fig, self.ax = plt.subplots()
-        # plt.ion()
         self.v1 = []
         self.v2 = []
         self.t = []
 
-        self.p1, = plt.plot([], [], 'ro')
-        self.p2, = plt.plot([], [], 'ro')
+        self.p1, = plt.plot([], [], 'r.', label='x-direction motor')
+        self.p2, = plt.plot([], [], 'b.', label='y-direction motor')
         self.data1 = 0.0
         self.data2 = 0.0
 
     def initFigure(self):
         self.ax.set_xlim(0, 25)
         self.ax.set_ylim(0, 11000)
+        self.ax.set_xlabel("time (s)")
+        self.ax.set_ylabel("degree")
 
     def sendDate(self, v1, v2):
         self.data1 = v1
         self.data2 = v2
 
     def update(self, frame):
-        # self.ax.set_xlim(-1, 50)
         self.v1.append(self.data1)
         self.v2.append(self.data2)
         self.t.append(time.time() - self.now)
@@ -45,7 +47,19 @@ class MonitorPlot():
                             init_func=self.initFigure,
                             interval=100,
                             repeat=False)
-        # plt.legend()
+        plt.legend()
         plt.grid()
         plt.show()
 
+
+def update():
+    while True:
+        ani.sendDate(1000, 3000)
+        time.sleep(0.1)
+
+
+if __name__ == "__main__":
+    global ani
+    ani = MonitorPlot()
+    threading.Thread(target=update).start()
+    ani.animate()
